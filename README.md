e2a86ec3f93a2e051d5cb5581a651cbfa37ab6b4
Edward Delaporte <edward@delaporte.us>
2019-12-03 11:32:26 -0600

    Updated bootstrap for WSL

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 My Linux dot files.
 
 # Setup
-Copy the file `bootstrap.sh` to the destination system.
+Copy the file `setup.sh` to the destination system.
 Source it there.
 
-Make sure your shell is set to Zshell.
+Make sure your shell is set to PowerShell
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,33 +8,57 @@
 # You can scp just this file to the destination host,
 #   and then run it.
 
-if [ ! -f ~/.ssh/id_rsa ]; then
-	ssh-keygen
-fi
+# sudo apt-get install zsh
+# TODO: CURL me...
 
+## Setup Git and Vim
 if [ ! -d ~/dotfiles ]; then
+	sudo apt-get install git install vim
+	# TODO: Consider vim-nox to resolve 'needs python' error from some of my vundle plugins.
+	git config --global user.email "edward@delaporte.us"
+	git config --global user.name "Edward Delaporte"
+	git config --global core.editor /usr/bin/vim
 	cd ~; git clone git@github.com:edthedev/dotfiles.git
-	cp ~/dotfiles/id_rsa.pub ~/id_rsa.pub
-	chmod go+r ~/id_rsa.pub
 fi
-
-if [ ! -f ~/.zshrc ]; then
-	ln -s ~/dotfiles/zshrc ~/.zshrc
-fi
-
-if [ ! -f ~/.screenrc ]; then
-	ln -s ~/dotfiles/screenrc ~/.screenrc
-fi
-
-if [ ! -f ~/dotfiles/antigen/README.md ]; then
-	cd ~/dotfiles;git submodule update --init
-fi
-
 if [ ! -f ~/.vimrc ]; then
 	ln -s ~/dotfiles/vimrc ~/.vimrc
 fi
 
+## Setup PowerShell
+if [ ! -f ~/.config/powershell/Microsoft.PowerShell_profile.ps1 ]; then
+	sudo apt-get install powershell
+	chsh --shell /usr/bin/pwsh
+	mkdir -p /home/delaport/.config/powershell/
+	ln -s ~/dotfiles/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1 
+fi
+
+## Create SSH Keys
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen
+fi
+
+## Setup Remote Access
+# if [ ! -f ~/id_rsa.pub ]; then
+# 	cp ~/dotfiles/id_rsa.pub ~/id_rsa.pub
+#	chmod go+r ~/id_rsa.pub
+# fi
+
+## Setup ZShell Profile
+# if [ ! -f ~/.zshrc ]; then
+# 	ln -s ~/dotfiles/zshrc ~/.zshrc
+# fi
+# if [ ! -f ~/dotfiles/antigen/README.md ]; then
+# 	cd ~/dotfiles;git submodule update --init
+# fi
+
+## Setup Screen
+if [ ! -f ~/.screenrc ]; then
+	sudo apt-get install screen
+	ln -s ~/dotfiles/screenrc ~/.screenrc
+fi
+
+## Setup Vundle for Vim
 if [ ! -d ~/.vim/bundle ]; then
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ 	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	vim +BundleInstall +qall
 fi
