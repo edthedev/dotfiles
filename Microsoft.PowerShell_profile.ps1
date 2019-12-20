15e9b9bb734c36bebf7ad2bd5f0b2b30c972b9b8
Edward Delaporte <delaport@illinois.edu>
2019-12-20 10:28:18 -0600

    Let us answer 'why'

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,11 @@
 
-# Path to Flutter
-$ENV:PATH +=":/home/delaport/development/flutter/bin" # Kali under WSL
-
 # Some Paths that are annoying to find/restore if the installer fails
-$ENV:PATH+=";C:\Program Files\Python38\Scripts\;C:\Program Files\Python38\"
-$ENV:PATH+=";C:\ProgramData\DockerDesktop\version-bin;C:\Program Files\Docker\Docker\Resources\bin"
-$ENV:PATH+=";C:\Program Files (x86)\Common Files\Oracle\Java\javapath"
-$ENV:PATH+=";C:\Program Files\Microsoft VS Code\bin"
-$ENV:PATH+=";C:\Program Files\Git\cmd"
-$ENV:PATH+=";C:\ProgramData\chocolatey\bin"
+$ENV:PATH+=";C:\Program Files\Microsoft VS Code\bin" # One Editor to rule them all
+$ENV:PATH +=":/home/delaport/development/flutter/bin" # Flutter in Kali under WSL
+$ENV:PATH+=";C:\Program Files\Python38\Scripts\;C:\Program Files\Python38\" # Python latest as of 2019 December
+$ENV:PATH+=";C:\ProgramData\DockerDesktop\version-bin;C:\Program Files\Docker\Docker\Resources\bin" # Docker for R Studio and Jupyter
+$ENV:PATH+=";C:\Program Files (x86)\Common Files\Oracle\Java\javapath" # Don't remember why I still care about Java.
+$ENV:PATH+=";C:\Program Files\Git\cmd" # Version control is nice.
+$ENV:PATH+=";C:\ProgramData\chocolatey\bin" # Package management is nice.
 $ENV:PATH+=";C:\Program Files\nodejs" # KataKoda wants to add tools via npm
 $ENV:PATH+=";C:\PENGUIN" # Flag to ensure my profile kicked in.
