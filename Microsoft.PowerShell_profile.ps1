deea3a86481c845e55e46d90ff79620f878dabf4
Edward Delaporte <delaport@illinois.edu>
2019-12-23 09:37:52 -0600

    Trying to tweak Vault helper function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,9 @@
 
+# Kali linux env
+$ENV:PATH +=":/home/delaport/development/flutter/bin" # Flutter in Kali under WSL
+
 # Some Paths that are annoying to find/restore if the installer fails
 $ENV:PATH+=";C:\Program Files\Microsoft VS Code\bin" # One Editor to rule them all
-$ENV:PATH +=":/home/delaport/development/flutter/bin" # Flutter in Kali under WSL
 $ENV:PATH+=";C:\Program Files\Python38\Scripts\;C:\Program Files\Python38\" # Python latest as of 2019 December
 $ENV:PATH+=";C:\ProgramData\DockerDesktop\version-bin;C:\Program Files\Docker\Docker\Resources\bin" # Docker for R Studio and Jupyter
 $ENV:PATH+=";C:\Program Files (x86)\Common Files\Oracle\Java\javapath" # Don't remember why I still care about Java.
@@ -12,5 +14,5 @@ $ENV:PATH+=";C:\PENGUIN" # Flag to ensure my profile kicked in.
 
 function Launch-Vault() {
   consul agent -dev # Run in Dev Mode
-  vault sever -config c:\src\dotfiles\vaultconfig.hcl # Use local dev Consul as storage
+  vault sever -config c:\src\dotfiles\vaultconfig.hcl -address=http://127.0.0.1:8200 # Use local dev Consul as storage
 }
