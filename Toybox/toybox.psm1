94c74d75f44a0be22e2d3bb7f06947e596bc625f
Edward Delaporte <delaport@illinois.edu>
2020-03-26 13:04:59 -0500

    Cleanup unused.

--- Toybox/toybox.psm1
+++ Toybox/toybox.psm1
@@ -1,24 +1,6 @@
 $TOYBOX_ROOT = "C:\src\dotfiles\toybox"
 $TOYBOX_DOCKER_NAME = "secops"
 
-function Show-Calendar {
-  Write-Host "Hello Calendar."
-      }
-Export-ModuleMember -Function Show-Calendar
-
-function New-DockerPython2 {
-  docker build $TOYBOX_ROOT
-  docker container create --name $TODYBOX_DOCKER_NAME e8cff83571c6
-}
-
-function Start-DockerPython2 {
-  docker start $TOYBOX_DOCKER_NAME
-}
-function Start-DockerPython2Shell {
-  docker exec $TOYBOX_DOCKER_NAME bash
-  # Set-Location $TOYBOX_ROOT; docker run
-}
-
 function Start-DockerPythonBash {
   docker run -it python /bin/bash
 }
