2e415313c1718d9a65085430c44708335bcd341b
Edward Delaporte <delaport@illinois.edu>
2020-03-26 13:05:10 -0500

    Cleanup comments.

--- Toybox/toybox.psm1
+++ Toybox/toybox.psm1
@@ -5,7 +5,4 @@ function Start-DockerPythonBash {
   docker run -it python /bin/bash
 }
 
-# Export-ModuleMember -Function New-DockerPython2
-# Export-ModuleMember -Function Start-DockerPython2
-# Export-ModuleMember -Function Start-DockerPython2Shell
 Export-ModuleMember -Function Start-DockerPythonBash
