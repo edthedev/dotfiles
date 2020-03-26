3c063b69039cc17714b62f8b1e6a3cea495339b7
Edward Delaporte <delaport@illinois.edu>
2020-03-26 13:04:41 -0500

    Messing with run commands.

--- Toybox/toybox.psm1
+++ Toybox/toybox.psm1
@@ -6,13 +6,24 @@ function Show-Calendar {
       }
 Export-ModuleMember -Function Show-Calendar
 
-function New-DockerPython26 {
+function New-DockerPython2 {
   docker build $TOYBOX_ROOT
   docker container create --name $TODYBOX_DOCKER_NAME e8cff83571c6
 }
 
-function Start-Python2 {
+function Start-DockerPython2 {
   docker start $TOYBOX_DOCKER_NAME
+}
+function Start-DockerPython2Shell {
   docker exec $TOYBOX_DOCKER_NAME bash
   # Set-Location $TOYBOX_ROOT; docker run
 }
+
+function Start-DockerPythonBash {
+  docker run -it python /bin/bash
+}
+
+# Export-ModuleMember -Function New-DockerPython2
+# Export-ModuleMember -Function Start-DockerPython2
+# Export-ModuleMember -Function Start-DockerPython2Shell
+Export-ModuleMember -Function Start-DockerPythonBash
