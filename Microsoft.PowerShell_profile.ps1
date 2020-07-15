221d984195390c9dc955082c1694e8e00db0c4fd
Edward Delaporte <delaport@illinois.edu>
2020-07-15 13:07:14 -0500

    ASCII helper.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -101,3 +101,58 @@ function Start-DockerFindSecrets {
   # Write-Host "Current folder mounted as /etc/src"
   docker run -v /c/src:/etc/src dxa4481/trufflehog /etc/src/SecOps-Tools
 }
+
+
+#requires -version 5.1
+#ConvertTo-ASCIIArt.ps1
+ 
+<#
+font list at https://artii.herokuapp.com/fonts_list
+font names are case-sensitive
+ 
+invoke-restmethod https://artii.herokuapp.com/fonts_list
+ 
+#>
+ 
+<#
+.SYNOPSIS
+Output strings as ASCII art fonts.
+
+.EXAMPLE
+cart "Testing"-Font cursive
+
+.NOTES
+From https://jdhitsolutions.com/blog/powershell/7278/friday-fun-powershell-ascii-art/
+#>
+Function ConvertTo-ASCIIArt {
+  [cmdletbinding()]
+  [alias("cart")]
+  [outputtype([System.String])]
+  Param(
+      [Parameter(Position = 0, Mandatory, HelpMessage = "Enter a short string of text to convert", ValueFromPipeline)]
+      [ValidateNotNullOrEmpty()]
+      [string]$Text,
+      [Parameter(Position = 1,HelpMessage = "Specify a font from https://artii.herokuapp.com/fonts_list. Font names are case-sensitive")]
+      [ValidateNotNullOrEmpty()]
+      [string]$Font = "big"
+  )
+
+  Begin {
+      Write-Verbose "[$((Get-Date).TimeofDay) BEGIN] Starting $($myinvocation.mycommand)"
+  } #begin
+
+  Process {
+      Write-Verbose "[$((Get-Date).TimeofDay) PROCESS] Processing $text with font $Font"
+      $testEncode = [uri]::EscapeDataString($Text)
+      $url = "http://artii.herokuapp.com/make?text=$testEncode&font=$Font"
+      Try {
+          Invoke-Restmethod -Uri $url -DisableKeepAlive -ErrorAction Stop
+      }
+      Catch {
+          Throw $_
+      }
+  } #process
+  End {
+      Write-Verbose "[$((Get-Date).TimeofDay) END    ] Ending $($myinvocation.mycommand)"
+  } #end
+}
