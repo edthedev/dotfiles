901d1fd436ac31830e88a6ac12bb43f237615082
EdTheDev <edward@delaporte.us>
2021-01-30 15:54:02 -0600

    Show hidden files.

--- setup/powershell.ps1
+++ setup/powershell.ps1
@@ -1,1 +1,7 @@
 Set-ExecutionPolicy RemoteSigned
+
+Show file extensions for known types - run once
+Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+Set-ItemProperty . HideFileExt "0"
+Set-Location C:\
+
