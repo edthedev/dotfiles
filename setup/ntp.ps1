0a5bc563f473a53e1af175e49a4c3982c5593e98
Edward Delaporte <delaport@illinois.edu>
2021-02-23 13:04:15 -0600

    Preferred NTP server.

--- setup/ntp.ps1
+++ setup/ntp.ps1
@@ -1,7 +1,7 @@
 w32tm /query /status
 
 net stop w32time
-w32tm /config /syncfromflags:manual /manualpeerlist:"0.it.pool.ntp.org 1.it.pool.ntp.org 2.it.pool.ntp.org 3.it.pool.ntp.org"
+w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.illinois.edu 0.it.pool.ntp.org 1.it.pool.ntp.org 2.it.pool.ntp.org 3.it.pool.ntp.org"
 net start w32time
 w32tm /config /update
 w32tm /resync /rediscover
