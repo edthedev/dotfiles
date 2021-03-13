80d3b3ade1b5c65ec816184ff69e10b0f2e3f4a5
EdTheDev <edward@delaporte.us>
2021-03-13 15:46:30 -0600

    Powershell 7 for Pi

--- install/powershell.sh
+++ install/powershell.sh
@@ -1,8 +1,4 @@
-## Setup PowerShell
 if [ ! -f ~/.config/powershell/Microsoft.PowerShell_profile.ps1 ]; then
-	echo "************** Install PowerShell and setup profile"
+	echo "************** Install PowerShell"
 	sudo apt-get install powershell
-	chsh --shell /usr/bin/pwsh
-	mkdir -p ~/.config/powershell/
-	ln -s ~/dotfiles/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1 
 fi
---
+++ setup/powershell.sh
@@ -0,0 +1,6 @@
+if [ ! -f ~/.config/powershell/Microsoft.PowerShell_profile.ps1 ]; then
+	echo "************** Link PowerShell profile"
+	chsh --shell /usr/bin/pwsh
+	mkdir -p ~/.config/powershell/
+	ln -s ~/dotfiles/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1 
+fi
