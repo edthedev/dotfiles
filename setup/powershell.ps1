d95943b9389841446d51c4330e82f69079690914
EdTheDev <edward@delaporte.us>
2021-03-13 15:48:02 -0600

    Better file names.

--- setup/powershell.ps1
+++ setup/powershell.ps1
@@ -1,8 +1,2 @@
 Write-Host "Require remote scripts to be signed."
 Set-ExecutionPolicy RemoteSigned
-
-Write-Host "Show file extensions for known types"
-Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
-Set-ItemProperty . HideFileExt "0"
-Set-Location C:\
-
