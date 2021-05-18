09835a123eb4e1e010c551057f4f8203dfd66b15
Edward Delaporte <delaport@illinois.edu>
2021-05-18 10:38:06 -0500

    Fix for rm command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -26,6 +26,7 @@ if($IsWindows){
 	$ENV:PATH+=";C:\Program Files\Microsoft VS Code\bin" # One Editor to rule them all
 	$ENV:PATH+=";C:\Program Files\Git\cmd" # Version control is nice.
 	$ENV:PATH+=";C:\ProgramData\chocolatey\bin" # Package management is nice.
+	$ENV:PATH+=";C:\Program Files (x86)\GnuWin32\bin" # GNU Utils - i.e. rm
 	$ENV:PATH+=";C:\PENGUIN" # Flag to ensure my profile kicked in.
 }
 
