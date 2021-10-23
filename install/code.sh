158b476f40b9b827a82c38a877fdadd1de2dffd4
Edward Delaporte <edward@delaporte.us>
2021-10-23 05:44:18 +0100

    Simpler VSCode install. Works on Pi

--- install/code.sh
+++ install/code.sh
@@ -1,8 +1,1 @@
-if [ ! -d ~/.installed/code ]; then
-	echo "************** Install Visual Studio Code"
-	# sudo apt install snapd
-	# sudo snap install --classic code
-	wget https://packagecloud.io/headmelted/codebuilds/gpgkey -O - | sudo apt-key add -
-  curl -L https://raw.githubusercontent.com/headmelted/codebuilds/master/docs/installers/apt.sh | sudo bash
-	touch ~/.installed/code
-fi
+apt install code
