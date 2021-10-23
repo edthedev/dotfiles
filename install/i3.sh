a5cf71f9ebef1f35b4f3ddfbc50dcebe2193796d
Edward Delaporte <edward@delaporte.us>
2021-10-23 05:55:58 +0100

    Fix

--- install/i3.sh
+++ install/i3.sh
@@ -1,4 +1,6 @@
 echo "************** Install i3 Window Manager"
 sudo apt-get install i3 lightdm
-echo "************** Install Chromium
+echo "************** Install Chromium"
 sudo apt-get install chromium
+echo "************** Install Tilix Terminal"
+sudo apt-get install tilix
