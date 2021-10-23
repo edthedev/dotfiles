7517cc10a1bea7f6226e5748591b404252f589f9
Edward Delaporte <edward@delaporte.us>
2021-10-23 04:59:39 +0100

    working to make i3 tolerable

--- install/i3.sh
+++ install/i3.sh
@@ -1,5 +1,4 @@
-if [ ! -f ~/.installed/i3 ]; then
-	echo "************** Install i3 Window Manager"
-	sudo apt-get install i3
-	touch ~/.installed/i3
-fi
+echo "************** Install i3 Window Manager"
+sudo apt-get install i3 lightdm
+echo "************** Install Chromium
+sudo apt-get install chromium
