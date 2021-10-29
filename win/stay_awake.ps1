c656b6f73876cfbfa323bdc06e08d87e6089c1e6
Edward Delaporte <edward@delaporte.us>
2021-10-29 03:53:07 +0100

    stay awake

---
+++ win/stay_awake.ps1
@@ -0,0 +1,9 @@
+$wsh = New-Object -ComObject WScript.Shell
+$minutes = 15
+do{
+    $minutes-=1;
+  # Send Shift+F15
+  $wsh.SendKeys('+{F15}')
+  Start-Sleep -seconds 59
+}until($minutes < 0)
+# Thank you to http://www.zhornsoftware.co.uk/caffeine/
