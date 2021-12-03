02605aa59c6c0b6e04df49069d44643bf4643bec
Edward Delaporte <delaport@illinois.edu>
2021-12-03 13:50:26 -0600

    Just handle charts here.

--- psmodules/sprint.psm1
+++ psmodules/sprint.psm1
@@ -1,10 +1,5 @@
 
-function Show-SprintPlan(){
-    param(
-        [switch]$list = $false,
-        [switch]$chart = $false
-    )
-    Write-Host "## Stats"
+function Show-SprintCharts(){
     $closed = Get-GHClosed
     $closed_count = ($closed | Measure-Object).Count
 	Add-Content -Path ~/.chart_closed -Value "|$closed_count"
@@ -20,27 +15,19 @@ function Show-SprintPlan(){
 	Add-Content -Path ~/.chart_unsized -Value "|$unsized_count"
 	$env:unsized =  Get-Content -Path ~/.chart_unsized
 
-    if($chart) {
-        chart -var "closed"
-    }
-    Write-Host "Closed Issues this Sprint: $closed_count"
-    if($chart) {
-        chart -var "unsized"
-    }
-    Write-Host "Count of Unsized Issues: $unsized_count"
-    if($chart) {
-        chart -var "orphans"
-    }
-    Write-Host "Count of Issues with no milestone: $orphans_count"
-
-    if($list) {
-        Write-Host "## Closed Issues Updated this Sprint (Show-GHClosed)"
-        $closed | Show-GHIssuesAsMarkdown
-        Write-Host "## Unsized Issues (Show-GHUnsized)"
-        $unsized | Show-GHIssuesAsMarkdown
-        Write-Host "## Issues with No Milestone"
-        $orphans | Show-GHIssuesAsMarkdown
-    }
+    Write-Host "## Closed Issues this Sprint: $closed_count"
+    Write-Host "'''sh"
+    chart -var "closed"
+    Write-Host "''' "
+    Write-Host "## Count of Unsized Issues: $unsized_count"
+    Write-Host "'''sh"
+    chart -var "unsized"
+    Write-Host "''' "
+    Write-Host "## Count of Issues with no milestone: $orphans_count"
+    Write-Host "'''sh"
+    chart -var "orphans"
+    Write-Host "''' "
 
 }
-Export-ModuleMember -Function Show-SprintPlan
+
+Export-ModuleMember -Function Show-SprintCharts
