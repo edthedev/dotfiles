bdade147187076b7af3b5651851cfd02cc36beae
Edward Delaporte <delaport@illinois.edu>
2022-12-22 08:56:06 -0600

    Add files folder

--- setup/bash.sh
+++ setup/bash.sh
@@ -1,2 +1,5 @@
-ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/files/bashrc ~/.bashrc
+ln -s ~/dotfiles/files/profile ~/.profile
+ln -s ~/dotfiles/files/screenrc ~/.screenrc
+ln -s ~/dotfiles/files/vimrc ~/.vimrc
 ls -al ~/ | grep bash
