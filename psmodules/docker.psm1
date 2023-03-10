c4f2ee0efe9c1b3f5c44d98e33e645dd59af50d5
Edward Delaporte <delaport@illinois.edu>
2023-03-10 10:06:09 -0600

    Updates to docker commands

--- psmodules/docker.psm1
+++ psmodules/docker.psm1
@@ -4,8 +4,8 @@
 Spin up a quick Python docker container.
 
 #>
-function Start-DockerPython{
-  docker run -it python /bin/bash
+function Invoke-DockerPython{
+  Invoke-Docker python
 }
 
 Export-ModuleMember -Function Start-DockerPython
@@ -14,3 +14,23 @@ Export-ModuleMember -Function Start-DockerPython
 #  code --list-extensions | Out-File -FilePath /src/dotfiles/vscode-extensions.txt
 #  Get-Content -Path /src/dotfiles/vscode-extensions.txt
 #}
+
+function Invoke-DockerData{
+  # docker run -i -t -v c:\data:/data:z civisanalytics/datascience-python:latest /bin/bash
+  docker run -it --rm -p 10000:8888 -v c:\data:/home/jovyan/work:z jupyter/datascience-notebook
+}
+
+function Invoke-DockerKali{
+  Invoke-Docker kalilinux/kali-rolling
+}
+
+function Invoke-Docker {
+  param(
+    [string]$version = kalilinux/kali-rolling
+  )
+  docker run -it -v c:\data:/data $version bash
+}
+
+function Invoke-DockerOWASPJuiceShop {
+  docker run --rm -p 3000:3000 bkimminich/juice-shop
+}
