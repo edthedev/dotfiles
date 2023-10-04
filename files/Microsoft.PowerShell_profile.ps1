9c2c4a0b2ea074b3e49b260cf4557abcf0ccc45a
Edward Delaporte <delaport@illinois.edu>
2023-10-04 16:40:54 -0500

    Quiet message

--- files/Microsoft.PowerShell_profile.ps1
+++ files/Microsoft.PowerShell_profile.ps1
@@ -139,4 +139,4 @@ function Invoke-FixWslVPN {
 	Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect Secure Mobility Client Virtual Miniport Adapter for Windows x64"} | Set-NetIPInterface -InterfaceMetric 6000
 }
 
-Write-Host "Real profile file is at $env:realProfile ($$env:realProfile)"
+# Write-Host "Real profile file is at $env:realProfile ($$env:realProfile)"
