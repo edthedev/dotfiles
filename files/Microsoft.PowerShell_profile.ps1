c3f4857ab04c9caa7e10a4b40139fbfa93b8a08f
Edward Delaporte <delaport@illinois.edu>
2023-10-06 11:03:25 -0500

    Down to one page of PSProfile!

--- files/Microsoft.PowerShell_profile.ps1
+++ files/Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,9 @@
-#WebDriverException: Message: nknown error: cannot determine loading status .EXAMPLE
-# . C:\src\dotfiles\Microsoft.PowerShell_profile.ps1
-
-# Courtesty of MKletz
-# Get-ChildItem -Path 'C:\Repos' -Filter "*.psd1" -Recurse | ForEach-Object -Process {
-#     $Path = Split-Path -Path $_.Directory -Parent
-#     $env:PSModulePath = $env:PSModulePath + ";$($Path)"
-#}
-# $env:PSModulePath = $env:PSModulePath.Replace("\\ad.uillinois.edu\techsvc\home\$($ENV:USERNAME)\Documents\PowerShell\Modules;",'')
-
-function Add-ToProfile {
-    param (
-        $Path
-    )
-	$profileContents = [string]::join([environment]::newline, (get-content -path $Path))
-	invoke-expression $profileContents
-}
-
 
 $env:src = "~\src"
-if($IsWindows){
-  $env:src = "c:\src"
-}
-
+Import-Module $env:src\dotfiles\psmodules\add_to_profile.psm1
 Add-ToProfile $env:src\dotfiles\files\paths.ps1
 Add-ToProfile $env:src\dotfiles\files\env.ps1
+Add-ToProfile $env:src\dotfiles\files\load_modules.ps1
 
 # You can pry my unix commands out of my cold dead fingers
 Set-Alias which get-command
@@ -40,37 +20,10 @@ function Enable-PoshGit {
 New-Alias pg    Enable-PoshGit
 Write-Host "+ Type 'pg' to enable PoshGit"
 
-
+# Let's face it, I'm about to cd into my source directory
 if((Get-Location).Path -eq $HOME){
 	cd $env:src
 }
-
-function Get-GitLog() {
-	git log --oneline
-}
-
-function Get-GitStatus() {
-	git status -b --short
-}
-
-# Tell my scripts which GitHub repositories to look in.
-$env:GITHUB_USERNAME = 'edthedev'
-$env:GITHUB_REPOS = @(
-	'techservicesillinois/SecOps-Tools', 
-	'techservicesillinois/secdev-job-aids', 
-	'techservicesillinois/awscli-login', 
-	'techservicesillinois/farmit',
-	'uillinois-community/uillinois-community.github.io',
-	'techservicesillinois/SecOps-Powershell-CSOC',
-	'techservicesillinois/SecOps-Powershell-CISDSC',
-	'techservicesillinois/secops-splunk-null-router'
-) -join ' '
-$ENV:TS_REPOS = $ENV:GITHUB_REPOS.split(' ') | Where { $_ -like 'techser*' }
-
-
-# Nice for git
-New-Alias ol 		Get-GitLog
-New-Alias st 		Get-GitStatus
 
 # Dashboard
 New-Alias dash   Show-MyDashboard # from dash.psm1
@@ -79,9 +32,4 @@ function Get-JournalAgenda() {
 	agenda.exe -path "$HOME/Journal/{YYYY}/{MM}-{DD}.md"
 }
 New-Alias today Get-JournalAgenda
-
-function Invoke-FixWslVPN {
-	Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect Secure Mobility Client Virtual Miniport Adapter for Windows x64"} | Set-NetIPInterface -InterfaceMetric 6000
-}
-
 # Write-Host "Real profile file is at $env:realProfile ($$env:realProfile)"
