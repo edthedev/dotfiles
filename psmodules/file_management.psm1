a5613ec6500de3fc6e8acbc2d1364d10886cfedf
EdTheDev <edward@delaporte.us>
2023-11-11 16:49:00 -0600

    Update stay awake as function

--- psmodules/file_management.psm1
+++ psmodules/file_management.psm1
@@ -42,3 +42,20 @@ Get-Job -Name "FileCollection" | Remove-Job
 
 Export-ModuleMember -Function Show-DriveSpace 
 Export-ModuleMember -Function Show-LargeFiles
+
+# Stay awake to allow large transfers to complete
+#    without permanently giving up battery saving features
+function Invoke-StayAwake() {
+	param($minutes=90)
+	$wsh = New-Object -ComObject WScript.Shell
+	do{
+		"Staying awake for $minutes minutes"
+	$minutes-=1;
+	# Send Shift+F15
+	$wsh.SendKeys('+{F15}')
+	Start-Sleep -seconds 59
+	}until($minutes -lt 0)
+	# Thank you to http://www.zhornsoftware.co.uk/caffeine/
+}
+
+Export-ModuleMember -Function Invoke-StayAwake
