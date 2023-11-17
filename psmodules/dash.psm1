a3bbadc19a551b791371267957e72541a7c25073
Edward Delaporte <delaport@illinois.edu>
2023-11-17 16:47:30 -0600

    Removed unused ashboard bits

--- psmodules/dash.psm1
+++ psmodules/dash.psm1
@@ -1,25 +1,26 @@
+function Get-JournalAgenda() {
+    agenda.exe -path "$HOME/Journal/{YYYY}/{MM}-{DD}.md"
+}
 
 function Show-MyDashBoard() {
 	Write-Host ""
 	Write-Host "## Todo items for today:"
-	Measure-JournalTodos
+	# Measure-JournalTodos
 	chart
-	Get-JournalTodayTodos
+	# Get-JournalTodayTodos
 	Write-Host ""
 	Write-Host "## Agenda for today:"
 	Get-JournalAgenda
 	Write-Host ""
 	Write-Host "## GitHub Issues Assigned (Show-AgileMine)"
 	# Show-AgileMine
-	Show-AgileMine -DaysAgo 6
+	# Show-AgileMine -DaysAgo 6
 	Write-Host ""
 	Write-Host "## GitHub Issues with no milestone. (Select-AgileNoMilestone)"
 	Write-Host "(Get-AgileQuery -state 'Open' | Invoke-AgileQuery | Select-AgileNoMilestone | Show-MarkdownFromGitHub)"
 	$milestone_repos = $env:github_repos.split(' ') | Where-Object { $_ -Match 'techservicesillinois' }
-    $queries = Get-AgileQuery -state 'Open' -repos $milestone_repos
-	Invoke-AgileQuery -queries $queries | Select-AgileNoMilestone | Show-MarkdownFromGitHub
-    # $orphans_count = ($orphans | Measure-Object -Property updated_at -Min -Max).Count
-	# $orphans | Measure-Object -Property updated_at -Min -Max
+    # $queries = Get-AgileQuery -state 'Open' -repos $milestone_repos
+	# Invoke-AgileQuery -queries $queries | Select-AgileNoMilestone | Show-MarkdownFromGitHub
 	Write-Host ""
 	Write-Host "Use command 'todo' to list more tasks."
 	Write-Host "Use command 'chart' to show todo item progress."
