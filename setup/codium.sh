347342c1b178e31a00d8f51c1a09bddbc252d857
EdTheDev <edward@delaporte.us>
2023-12-03 00:23:39 -0600

    Consolidate VSCodium setup

--- setup/codium.sh
+++ setup/codium.sh
@@ -1,17 +1,18 @@
+rm ~/.config/VSCodium/User/settings.json
+ln -s ~/dotfiles/files/vscodium/settings.json ~/.config/VSCodium/User/settings.json
+
+rm ~/.config/VSCodium/User/keybindings.json
+ln -s ~/dotfiles/files/vscodium/keybindings.json ~/.config/VSCodium/User/keybindings.json
+
+
+codium --install-extension benjpas.close-all
+codium --install-extension vscodevim.vim
+codium --install-extension pajoma.vscode-journal
 codium --install-extension ban.spellright
+# codium --install-extension tomasr8.close-all-but-this
 codium --install-extension bierner.markdown-mermaid
-codium --install-extension brunnerh.insert-unicodium
-codium --install-extension ms-vscodium-remote.remote-containers
-codium --install-extension ms-vscodium-remote.remote-ssh
-codium --install-extension ms-vscodium-remote.remote-ssh-edit
-codium --install-extension ms-vscodium-remote.remote-wsl
-codium --install-extension ms-vscodium-remote.vscodium-remote-extensionpack
-codium --install-extension ms-vscodium.remote-explorer
-codium --install-extension ms-vscodium.remote-server
-codium --install-extension pajoma.vscodium-journal
-codium --install-extension rioj7.vscodium-json-validate
-codium --install-extension tomasr8.close-all-but-this
-codium --install-extension vscodiumvim.vim
-codium --install-extension yzane.markdown-pdf
-codium --install-extension yzhang.markdown-all-in-one
-codium --install-extension ZainChen.json
+# codium --install-extension brunnerh.insert-unicodium
+codium --install-extension ms-vscode-remote.vscode-remote-extensionpack
+# codium --install-extension rioj7.vscodium-json-validate
+# codium --install-extension yzhang.markdown-all-in-one
+# codium --install-extension ZainChen.json
