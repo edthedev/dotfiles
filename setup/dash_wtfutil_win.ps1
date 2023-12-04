b74fcdb3096c11d8989b71545aac14f0a2a7c637
Edward Delaporte <delaport@illinois.edu>
2023-12-04 07:42:18 -0600

    WIP - setting up dashboard

---
+++ setup/dash_wtfutil_win.ps1
@@ -0,0 +1,6 @@
+# mkdir -p ~/.config/wtf
+rm ~/.config/wtf/config.yml
+cp ~/dotfiles/files/wtfutil.config.yml ~/.config/wtf/config.yml
+
+# mkdir -p ~/.todo
+cp ~/dotfiles/files/todo-txt.config ~/.todo/config
