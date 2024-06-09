4632fd2f82b97c5f373330527ac35cd85a12277d
Edward Delaporte <edward@delaporte.us>
2024-06-08 23:06:12 -0500

    Check multiple files

---
+++ files/check_backups.sh
@@ -0,0 +1,7 @@
+# Requires github.com/edthedev/checkback
+checkback -path /mnt/nas/home/Backup/blaster/home/edward/minetest/worlds/SurfaceWorld/map.sqlite
+echo ""
+checkback -path /mnt/nas/home/Backup/blaster/home/edward/minetest/worlds/SurfaceWorld/map.sqlite
+echo ""
+checkback -path /mnt/nas/home/Backup/blaster/home/edward/minetest/worlds/SurfaceWorld/map.sqlite
+echo ""
