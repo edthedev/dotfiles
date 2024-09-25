961fb1fe925ce4c61e399a94ac225b94c2fda8d7
Edward Delaporte <edward@delaporte.us>
2024-09-24 23:23:57 -0500

    Update to latest MineTest

--- invoke/minetest/tasks.py
+++ invoke/minetest/tasks.py
@@ -8,11 +8,17 @@ def debug(c):
 
 @task
 def install(c):
+    print("Installing...")
+    # result = c.run('which minetest')
+    # if not(result.ok):
+    c.run("sudo apt install flatpak")
+    c.run("sudo apt install gnome-software-plugin-flatpak")
+    c.run("flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
+    c.run("flatpak install flathub net.minetest.Minetest -y")
     result = c.run('which minetest')
-    if not(result.ok):
-        c.run("apt install minetest")
-    else:
-        print(f"MineTest installed at {result.stdout}")
+    print(f"MineTest installed at {result.stdout}")
+    print("To run: flatpak run net.minetest.Minetest")
+
 
 @task
 def restore(c, dir):
