226ddc66eb34f3cb155f6846045f6e391c84ff5b
Edward Delaporte <edward@delaporte.us>
2024-10-15 18:26:34 -0500

    Fix

--- files/init.lua
+++ files/init.lua
@@ -2,9 +2,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
--- Plug 'tpope/vim-sensible'
-Plug 'pprovost/vim-ps1'
-Plug 'vim-airline/vim-airline'
-Plug 'sheerun/vim-polyglot'
+Plug 'tpope/vim-sensible'
+-- Plug 'pprovost/vim-ps1'
+-- Plug 'vim-airline/vim-airline'
+-- Plug 'sheerun/vim-polyglot'
 
 vim.call('plug#end')
