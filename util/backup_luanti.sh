f455e0756a49f91d16426eb2b0073bfc2f01013c
Edward Delaporte <edward@delaporte.us>
2025-02-24 18:35:11 -0600

    Add reminder

--- util/backup_luanti.sh
+++ util/backup_luanti.sh
@@ -2,4 +2,6 @@ cd ~/ansible/luanti
 ./do_luanti.sh backup.sh
 echo "Checking Database file..."
 ./check_db.sh
-echo "Reminder: Verify backup to NAS"
+echo "Reminder: Verify backup to NAS."
+echo "Reminder: Restart the server."
+
