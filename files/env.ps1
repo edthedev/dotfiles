c97084ceef0154bd8d99fdfd1bfa8cb84ee1e70c
Edward Delaporte <delaport@illinois.edu>
2025-04-02 13:03:45 -0500

    More git repos

--- files/env.ps1
+++ files/env.ps1
@@ -1,11 +1,22 @@
-$env:Journal = "~\Box\2024.Journal" # allows cd $env:journal
-$env:todolist = "C:\Users\delaport\Box\2024.Journal"
-$ENV:todotxt = "$ENV:todolist\todo.txt"
-$ENV:todohist = "$ENV:todolist\todo.hist.csv"
+$env:todolist = $env:journal
+$ENV:todotxt = Join-Path $env:todolist "todo.txt"
+$ENV:todohist = Join-Path $env:todolist "todo.hist.csv"
 # Tell my scripts which GitHub repositories to look in.
 $env:GITHUB_USERNAME = 'edthedev'
-$env:GITHUB_CLONE_PATH = '~\src'
+$env:GITHUB_CLONE_PATH = Join-Path $env:USERPROFILE 'src'
 $env:GITHUB_REPOS = @(
+
+	'techservicesillinois/SecOps-Powershell-Axonius',
+	'techservicesillinois/SecOps-PowerShell-AzureRunbooks',
+	'techservicesillinois/secops-honeypot-serversetup',
+	'techservicesillinois/SecOps-Qualys-CDBSync',
+	'techservicesillinois/SecOps-Powershell-Qualys',
+	'techservicesillinois/SecOps-Powershell-TDXTickets',
+	'techservicesillinois/SecOps-Powershell-Splunk',
+	'techservicesillinois/SecOps-Powershell-Feroot',
+	'techservicesillinois/SecOps-PowerShell-CDB',
+	'techservicesillinois/secops-aws-qualysrss',
+	'techservicesillinois/SecOps-PowerShell-DMI',
 	'techservicesillinois/SecOps-Tools', 
 	'techservicesillinois/secdev-job-aids', 
 	'techservicesillinois/awscli-login', 
@@ -19,8 +30,20 @@ $env:GITHUB_REPOS = @(
 	'techservicesillinois/secops-splunk-illinois-midpoint',
 	'techservicesillinois/soar-clearpass',
 	'techservicesillinois/phantom-toolbox'
-
 ) -join ' '
 # 'uillinois-community/uillinois-community.github.io',
 $ENV:TS_REPOS = $ENV:GITHUB_REPOS.split(' ') | Where { $_ -like 'techser*' }
+$env:GITHUB_FOCUS_REPOS = @(
+	'techservicesillinois/SecOps-Tools', 
+	'techservicesillinois/secdev-job-aids', 
+	'techservicesillinois/awscli-login', 
+	'techservicesillinois/SecOps-Powershell-CSOC',
+	'techservicesillinois/SecOps-Powershell-CISDSC',
+	'techservicesillinois/SecOps-PowerShell-CISDSCTemplates',
+	'techservicesillinois/splunk-soar-template',
+	'techservicesillinois/secops-soar-tdx',
+	'techservicesillinois/secops-splunk-illinois-midpoint',
+	'techservicesillinois/soar-clearpass',
+	'techservicesillinois/phantom-toolbox'
 
+) -join ' '
